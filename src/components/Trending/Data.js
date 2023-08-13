9b0c85d975c1f7585308e7e4e64133b94e36b48a
bismarkboateng <bismarkb609@gmail.com>
2023-08-13 12:27:54 +0000

    implement trending component

---
+++ src/components/Trending/Data.js
@@ -0,0 +1,53 @@
+import { Profile } from "../../assets";
+
+
+export default [
+    {
+        profileImage: Profile,
+        Name: "Marie Le Conte",
+        Title: "Limbs",
+        Date: "Aug 9",
+        Duration: "4 min read",
+        index: 1,
+    },
+    {
+        profileImage: Profile,
+        Name: "Malky McEwan in The Writing Cooperative",
+        Title: "The Magic Art of Saying More with Less",
+        Date: "Aug 5",
+        Duration: "5 min read",
+        index: 2,
+    },
+    {
+        profileImage: Profile,
+        Name: "M.H. Williams in Into The Discourse",
+        Title: "X Marks The Spot Where Musk Buried Twitter's Brand",
+        Date: "July 31",
+        Duration: "6 min read",
+        index: 3,
+    },
+    {
+        profileImage: Profile,
+        Name: "Orrin Onken in Crow's Feet",
+        Title: "Retirement Hobbies I've Tried So You Don't Have To",
+        Date: "Aug 5",
+        Duration: "6 min read",
+        index: 4,
+    },
+    {
+        profileImage: Profile,
+        Name: "Esther Crawford",
+        Title: "An epilogue to my time working at Twitter",
+        Date: "July 27",
+        Duration: "10 min read",
+        index: 5,
+    },
+    {
+        profileImage: Profile,
+        Name: "Stan Pugsley in Towards Data Science",
+        Title: "Why You Need a Knowledge Graph, And How to Build It",
+        Date: "Aug 9",
+        Duration: "7 min read",
+        index: 6,
+    }
+]
--- src/components/Trending/index.jsx
+++ src/components/Trending/index.jsx
@@ -1,16 +1,37 @@
 import styles from "./index.module.scss";
 import { PiTrendUpThin } from "react-icons/pi";
+import Data from "./Data";
+
 
 
 export default function index() {
   return (
     <section className={styles.trendingWrapper}>
       <div className={styles.trending}>
-
+        
         <div className={styles.trendingIcon}>
-          <i><PiTrendUpThin /></i>
-          <span>Trending on Medium</span>
+          <i><PiTrendUpThin /></i> 
+          <span className={styles.trendingText}>Trending on Medium</span>
         </div>
+
+        <ul className={styles.trendingListItem}>
+          {
+            Data.map((item) => (
+              <li key={item.index} className={styles.trendingItem}>
+                <div className={styles.trendingItemIndex} >0{item.index}</div>
+                <div className={styles.trendingItemContent}>
+                  <div>
+                    <img src={item.profileImage} alt={item.Title} className={styles.trendingItemImage} />  
+                    <div>{item.Name}</div>
+                  </div>
+                  <h4>{item.Title}</h4>
+                  <div className={styles.trendingItemDateTime}>{item.Date} . {item.Duration}</div>
+                </div>
+              </li>
+            ))
+          }
+        </ul>
+        
 
       </div>
     </section>
